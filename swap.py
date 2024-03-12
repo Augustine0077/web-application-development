40fda69f9356b4b8f75e6f85ba84f4edc1921797
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2024-03-12 13:54:56 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,9 @@
+#This program takes two inputs. The first input is stored in a variable called a. The second input is stored in a variable called b.
+# There are two variables, a and b from input
+a = input()
+b = input()
+temp=a
+a=b
+b=temp
+print("a: " + a)
+print("b: " + b)
